aab8ca7a7808fd2872bc3368d36f5ecadb7c59a6
Luis Gerardo <luisgcolmena@gmail.com>
2024-07-13 17:12:57 -0400

    Agregando los fetch para el texto y la imagen

--- vite-app-gatitos/src/App.jsx
+++ vite-app-gatitos/src/App.jsx
@@ -1,5 +1,30 @@
+import React, { useState, useEffect} from "react"
+
+const RANDOM_FACT = 'https://catfact.ninja/fact'
+const RANDOM_IMG = 'https://cataas.com/cat/says/sombri'
+
 export function App () {
+
+  const [fact, setFact] = useState('')
+  const [image, setImage] = useState('')
+
+  useEffect( () => {
+    fetch(RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+      
+    fetch(RANDOM_IMG)
+      .then(res => setImage(res.url))
+      /* .then(data => console.log(data))  */
+    }, [])
+
+
   return (
-    <h1>App de gatitos</h1>
+    <>
+      <h1>App de gatitos</h1>
+      {fact && <h3> { fact } </h3>}
+
+      {image && <img src= { image } width='600px' height=''/>}
+    </>
   )
 }
