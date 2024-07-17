edd78bd3607b0486d3d2cbb4f155ec3222f522dc
Luis Gerardo <luisgcolmena@gmail.com>
2024-07-16 22:03:17 -0400

    Corrigiendo las peticiones a la api y extrayendo las primeras 3 palabras del primer fetch

--- vite-app-gatitos/src/App.jsx
+++ vite-app-gatitos/src/App.jsx
@@ -1,30 +1,47 @@
 import React, { useState, useEffect} from "react"
 
 const RANDOM_FACT = 'https://catfact.ninja/fact'
-const RANDOM_IMG = 'https://cataas.com/cat/says/sombri'
+const RANDOM_IMG = ' https://cataas.com/cat/says/'
 
 export function App () {
 
   const [fact, setFact] = useState('')
-  const [image, setImage] = useState('')
+  const [imageUrl, setImageUrl] = useState('')
 
   useEffect( () => {
     fetch(RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => setFact(data.fact))
-      
-    fetch(RANDOM_IMG)
-      .then(res => setImage(res.url))
-      /* .then(data => console.log(data))  */
+      .then(res => {
+        /* const contentType = res.headers.get('content-type') */
+        return res.json()
+      })
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const firstThreeWords = fact.split(' ', 3).join(' ')
+        console.log(firstThreeWords)
+        
+        fetch(`${RANDOM_IMG}${firstThreeWords}`)
+          .then(res => {
+            /* const contentType = res.headers.get('content-type')
+            console.log(contentType) */
+            
+            const { url } = res
+            console.log(url)
+            setImageUrl(url)
+          })
+      })
+    
+       
     }, [])
 
 
   return (
-    <>
+    <main>
       <h1>App de gatitos</h1>
       {fact && <h3> { fact } </h3>}
 
-      {image && <img src= { image } width='600px' height=''/>}
-    </>
+      {imageUrl && <img src= { imageUrl } width='600px' height=''/>}
+    </main>
   )
 }
